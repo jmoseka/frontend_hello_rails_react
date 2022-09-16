e9574f0b6ca6166a843845a2282eb8d176080d56
jmoseka <jamylam3@gmail.com>
2022-09-16 20:46:36 +0300

    Add react app

---
+++ src/components/Greeting.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetingAsync } from '../store/greetingReducer';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const { greet } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(getGreetingAsync());
+  }, []);
+
+  const handleGreeting = () => {
+    dispatch(getGreetingAsync());
+  };
+
+  return (
+    <div className="div">
+      <h1>{greet}</h1>
+      <button type="button" onClick={handleGreeting}>Greeting</button>
+    </div>
+  );
+};
+
+export default Greeting;
